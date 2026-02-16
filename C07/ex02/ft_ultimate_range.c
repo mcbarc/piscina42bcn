9aa33e450bd59fefac9121814b41bc3f47e333cd
mcbarc <marc.costa.b@gmail.com>
2026-02-16 13:23:10 +0100

    updated ultimate rante

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -15,7 +15,7 @@
 
 int ft_ultimate_range(int **range, int min, int max)
 {
-    int	size;
+	int	size;
 	int	i;
 
 	if (min >= max)
@@ -25,40 +25,46 @@ int ft_ultimate_range(int **range, int min, int max)
 	}
 	size = max - min;
 	*range = NULL;
-	*range = (int *)malloc(sizeof(int) * size);
+	*range = (int *)malloc(sizeof(int) * size); //CPU looks at the value of range 0x123 (the 
+						    //memory we crated in main). * tells CPU go to 
+						    //that address and change the value stored there.
+						    // And malloc gives you a new block of memory and
+						    // stores its address here, 0xABC.
 	if (!*range)
 		return (-1);
 	i = 0;
 	while (i < size)
 	{
-		(*range)[i] = min + i;
+		(*range)[i] = min + i; // (*range) says Go to 0x123 and see what addres is written 
+				       // there: 0xABC. Now go to that address and chang the value
+				       // it holds
 		i++;
 	}
-    return (size);
+	return (size);
 }
 
 int main(void)
 {
-	int *range;
-    int size;
+	int *range;  // CPU reserves lets say 0x123 here. But no value assignation yet
+	int size;
 	int	value;
-    int min;
-    int max;
+	int min;
+	int max;
 	int i;
 	
 	min = 7;
 	max = 18;
-    size = max - min;
-    value = ft_ultimate_range(&range, min, max);
-    if (!range)
-        return (1);
-    i = 0;
-    while (i < size)
-    {
+	size = max - min;
+	value = ft_ultimate_range(&range, min, max); //passing the empty box 0x123 to the fn. 
+	if (value == -1 || value == 0)
+        	return (1);
+	i = 0;
+	while (i < size)
+	{
 		printf("%d___", value);
-        printf("%d\n", range[i]);
+		printf("%d\n", range[i]);
 		i++;
-    }
-    free(range);
-    return (0);
+	}
+	free(range);
+	return (0);
 }
